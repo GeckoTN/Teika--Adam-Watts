00411f24f645593f66c959740aadb2d170c52506
GeckoTN <geckotn@outlook.com>
2025-06-04 10:04:04 -0500

    Player Carrying
    The player now carries the ball and can release it with space.

--- Assets/Images/PlayerBehaviour.cs
+++ Assets/Images/PlayerBehaviour.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class PlayerBehaviour : MonoBehaviour
 {
-
     public float speed;
-    
+    public GameObject ball;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -24,6 +24,19 @@ public class NewMonoBehaviourScript : MonoBehaviour
             Vector3 newPosition = transform.position;
             newPosition.x = newPosition.x + (speed/60);
             transform.position = newPosition;
+        }
+
+        if (ball != null) {
+            Vector3 ballOffset = new Vector3(0f, -1f, 0f);
+            ball.transform.position = transform.position + ballOffset;
+            ball.GetComponent<PolygonCollider2D>().enabled = false;
+            ball.GetComponent<Rigidbody2D>().gravityScale = 0f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            ball.GetComponent<PolygonCollider2D>().enabled = true;
+            ball.GetComponent<Rigidbody2D>().gravityScale = 1f;
+            ball = null;
         }
         }
     }
