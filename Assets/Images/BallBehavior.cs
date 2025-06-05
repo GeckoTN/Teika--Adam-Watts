5eb1bdd5fead75e0938ffc02a2fa049e4777f1c6
GeckoTN <geckotn@outlook.com>
2025-06-05 14:31:04 -0500

    Merging
    Made it so that the balls can merge together in order

--- Assets/Images/BallBehavior.cs
+++ Assets/Images/BallBehavior.cs
@@ -6,15 +6,41 @@ public class BallBehavior : MonoBehaviour
     public float timeStart;
     public float timeThusFar;
 
+    public GameObject[] balls;
+    public int ballType;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         timeStart = Time.time;
+        balls = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerBehaviour>().balls;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Ball")) {
+            GameObject otherObject = collision.gameObject;
+            int otherObjectType = otherObject.GetComponent<BallBehavior>().ballType;
+            Debug.Log("Collided with " + otherObjectType);
+
+            if (ballType == otherObjectType && ballType != 10) {
+                if (transform.position.x > otherObject.transform.position.x || (transform.position.y > otherObject.transform.position.y && transform.position.x == otherObject.transform.position.x)) {
+                    GameObject newBall =
+                        Instantiate(balls[ballType+1], Vector3.Lerp(transform.position, otherObject.transform.position, 0.5f),Quaternion.identity);
+                    newBall.GetComponent<Collider2D>().enabled = true;
+                    newBall.GetComponent<Rigidbody2D>().gravityScale = 1f;
+                    Destroy(otherObject);
+                    Destroy(gameObject);
+                }
+            }
+        }
+        
+
         
     }
 
