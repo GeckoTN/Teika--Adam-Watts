7f9467a830d0b4f8cc80965757b7330ff9b3b0fa
GeckoTN <geckotn@outlook.com>
2025-06-05 10:27:44 -0500

    Bowling Ball Pretzel Animation
    Added and implemented an animation of the Bowling Ball eating a pretzel as an idle animation.

---
+++ Assets/Images/AnimationBehaviour.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class AnimationBehaviour : MonoBehaviour
+
+{
+    private Animator anim;
+
+    private float timer;
+    private float timerThreshold;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        timerThreshold = Random.Range(1f, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= timerThreshold) {
+            timer = 0f;
+            timerThreshold = Random.Range(1f, 3f);
+            
+            if (anim.GetBool("IsChewing") == true) {
+                anim.SetBool("IsChewing", false);
+            }
+            else {
+                anim.SetBool("IsChewing", true);
+            }
+        }
+    }
+}
